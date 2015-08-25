8a59b28cd118d4dc9c1f35e6293ef214308bf3ee
Tyler Guillen <tyguillen@gmail.com>
2015-08-24 17:11:45 -0700

    ttt working, needs refactoring for more functionality

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,56 +1,171 @@
 # When #new is called, initializes board, delegates which player is a computer and which player is a human, and plays the board.
+require 'enumerator'
+
 class TurnGame
   def initialize
     @board = TTTBoard.new
 
-    puts "PLAYER 1: "
-    @player_1 = player_prompt
-    puts "PLAYER 2: "
-    @player_2 = player_prompt
+    puts 'PLAYER 1: '
+    @player_1 = player_prompt(1)
+    puts 'PLAYER 2: '
+    @player_2 = player_prompt(2)
 
-    board.play(@player_1, @player_2)
+    puts @board.play(@player_1, @player_2)
   end
 
   # Helper method for deciding which players are computers or humans.
-  def player_prompt
-    puts "Human or Computer?"
+  def player_prompt(id)
+    puts 'Human or Computer?'
     choice = gets.chomp.downcase
-    if choice == "human"
-      player = HumanPlayer.new
-    elsif choice == "computer"
-      player = ComputerPlayer.new
+    if choice == 'human'
+      player = HumanPlayer.new(id)
+    elsif choice == 'computer'
+      player = ComputerPlayer.new(id)
     else
       puts "Invalid input.\n"
-      return player_prompt
+      return player_prompt(id)
     end
     player
   end
 end
 
-# Main tic-tac-toe class.
+# Plays the game.
 class TTTBoard
   def initialize
-    rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3]]
+    @rows = [
+        [7, 8, 9],
+        [4, 5, 6],
+        [1, 2, 3]
+    ]
+    @winning_positions = [[7, 4, 1], [8, 5, 2], [9, 6, 3], [1, 2, 3], [4, 5, 6], [7, 8, 9], [7, 5, 3], [9, 5, 1]]
   end
 
+  # Main method for gameplay. Randomizes the turn order and has players input moves until a win or a draw.
   def play(player_1, player_2)
-    
+    p_arr = randomize_players(player_1, player_2)
+    current_player = p_arr[0]
+    next_player = p_arr[1]
+    current_player.piece = 'X'
+    next_player.piece = 'O'
+    puts "Player #{current_player.id.to_s} goes first!"
 
+    end_message = ""
     loop do
-      display_board
+      set_cell(current_player.move(self))
+      if check_win?
+        end_message = current_player.win_message
+        break
+      elsif check_draw?
+        end_message = "It's a draw."
+        break
+      end
+      current_player, next_player = next_player, current_player
+    end
 
+    display_board
+    return end_message
+  end
+
+  # Displays the board in the command line.
+  def display_board
+    col_separator = ' | '
+    row_separator = '--+---+--'
+
+    @rows.each_with_index do |row, row_number|
+      row.each_with_index do |col, col_number|
+        print col.to_s
+        print col_separator unless col_number + 1 >= row.length
+      end
+      puts ''
+      puts row_separator unless row_number + 1 >= @rows.length
     end
   end
 
-  def display_board
+  # Sets the inputted cell on the board with the inputted piece.
+  def set_cell(args)
+    arr = @rows.flatten
+    arr[arr.find_index(args[:position])] = args[:piece]
+    @rows = arr.enum_for(:each_slice, 3).to_a
+  end
+
+  # Takes a position number and returns true if it is available for play.
+  def valid_position?(position)
+    arr = @rows.flatten
+    arr.find_index(position)
+  end
+
+  # Returns true if there are any rows full of Xs or Os
+  def check_win?
+    deconstruct = @rows +
+                  @rows.transpose +
+                  [[@rows[0][0], @rows[1][1], @rows[2][2]]] +
+                  [[@rows[0][2], @rows[1][1], @rows[2][0]]]
+
+    p deconstruct
+
+    deconstruct.any? do |line|
+      line.all? { |element| element == line[0] }
+    end
+  end
+
+  # Returns true if the board is full.
+  def check_draw?
+    @rows.all? do |row|
+      row.all? { |cell| cell == "X" || cell == "O" }
+    end
+  end
+
+  # Returns an array for a shuffled turn order.
+  def randomize_players(p1, p2)
+    r = rand(2)
+    a = []
+    if r == 0
+      a << p1
+      a << p2
+    else
+      a << p2
+      a << p1
+    end
+    a
+  end
+end
+
+# Plays each turn in the game as a Human, with human input.
+class HumanPlayer
+  attr_accessor :piece
+  attr_reader :id
+
+  def initialize(id)
+    @id = id
+  end
+
+  # Takes user input and returns the player's move.
+  def move(board)
+    board.display_board
+    print "Enter move for Player #{@id.to_s} (#{@piece}): "
+    position = gets.to_i
+    return {position: position, piece: @piece} if board.valid_position?(position)
+    puts "Invalid position."
+    move(board)
+  end
+
+  # This is displayed if the player wins.
+  def win_message
+    puts "Player #{@id.to_s} (#{@piece}) wins!"
+  end
+end
+
+class ComputerPlayer
+  attr_accessor :piece
+  attr_reader :id
+
+  def initialize(id)
+    @id = id
+  end
+
+  def move(board)
 
   end
 end
 
-class HumanPlayer
-
-end
-
-class ComputerPlayer
-
-end
+game = TurnGame.new
